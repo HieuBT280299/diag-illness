b488a8e501737234a468ba6f8593b231264b819d
Hieu Bui <hieubt280299@gmail.com>
2021-04-29 23:20:11 +0700

    change app name

--- src/shared/helper/index.tsx
+++ src/shared/helper/index.tsx
@@ -5,7 +5,7 @@ export const getHeaderTitle = (list: PageListItem[], path: string) => {
     const activeItem = list.find((item) => item.to === path);
     if (activeItem !== undefined) return activeItem.text;
   }
-  return "Illness Diagnosis System";
+  return "Hệ thống hỗ trợ chẩn đoán bệnh";
 };
 
 export const sortByName = (
